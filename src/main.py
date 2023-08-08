b9f7f95c372a3a3172c0907c270b3b58ae9ba138
Дмитрий <evans.d@yandex.ru>
2023-08-08 10:07:51 +0300

    Рефактор main.py

--- src/main.py
+++ src/main.py
@@ -2,34 +2,24 @@ import os
 
 from debug_toolbar.middleware import DebugToolbarMiddleware
 from fastapi import FastAPI
-from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
-from fastapi.openapi.utils import get_openapi
 
 from src.gitlab.routers import router as gitlab_router
 
 from .config import settings
 
-app = FastAPI(debug=os.getenv('DEBUG', False))
-
-app.add_middleware(
-    DebugToolbarMiddleware,
-    panels=['debug_toolbar.panels.sqlalchemy.SQLAlchemyPanel']
+app = FastAPI(
+    debug=os.getenv('DEBUG', False),
+    title='Matterlab',
+    version='0.0.1',
+    docs_url='/openapi'
 )
+app.openapi_version = '3.0.3'
+
+if settings.local:
+    app.add_middleware(
+        DebugToolbarMiddleware,
+        panels=['debug_toolbar.panels.sqlalchemy.SQLAlchemyPanel'],
+    )
 
 # Роутеры
 app.include_router(gitlab_router)
-
-
-# Настройка OpenAPI
-def custom_openapi():
-    openapi_schema = get_openapi(
-        title='Matterlab',
-        version='0.0.1',
-        openapi_version='3.0.3',
-        routes=app.routes
-    )
-    app.openapi_schema = openapi_schema
-    return app.openapi_schema
-
-
-app.openapi = custom_openapi
