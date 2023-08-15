af4ac6fa40ac3f6d3f1e90c29c49217a94abf00a
Дмитрий <evans.d@yandex.ru>
2023-08-15 10:21:46 +0300

    Рефактор интерфейса GitLabAPI

--- src/gitlab/api.py
+++ src/gitlab/api.py
@@ -9,9 +9,11 @@ from .exceptions import GitlabException
 
 
 class GitlabAPI:
+    """Интерфейс работы с API GitLab. https://docs.gitlab.com/ee/api/rest/"""
 
     def __init__(self, access_token: str):
-        self.base_url = 'https://gitlab.com/api/v4'
+        version = 'v4'
+        self.base_url = f'https://gitlab.com/api/{version}'
         self.headers = {'Authorization': f'Bearer {access_token}'}
 
     class Endpoints(enum.StrEnum):
@@ -21,18 +23,42 @@ class GitlabAPI:
         list_webhooks = create_webhook
         get_current_user = '/user'
 
-    def _get_url(self, endpoint: str) -> str:
+    @staticmethod
+    def _parse_response(response: httpx.Response) -> dict:
+        """
+        Обработка ответа с API
+        :param response: Объект класса Response, полученный после выполнения запроса (httpx._models.Response)
+        :return: Словарь данных ответа
+        """
+        response = response.json()
+        if response.status_code >= 400:
+            raise GitlabException(response)
+        return response
+
+    def _get_url(self, endpoint: Endpoints) -> str:
+        """
+        Формирование URL запроса
+        :param endpoint: объект из набора Endpoints (src.gitlab.api.GitlabAPI.Endpoints)
+        :return: готовый URL
+        """
         return f'{str(self.base_url)}{endpoint}'
 
     async def get_current_user(self) -> schemas.GitlabUser:
+        """
+        Получение информации о пользователе, кому принадлежит access_token
+        :return: Объект схемы GitlabUser (src.gitlab.schemas.GitlabUser)
+        """
+        url = self._get_url(self.Endpoints.get_current_user)
         async with httpx.AsyncClient() as client:
-            response = await client.get(self._get_url(self.Endpoints.get_current_user), headers=self.headers)
-        if response.status_code >= 400:
-            raise GitlabException
-        response = response.json()
+            response = await client.get(url, headers=self.headers)
+        response = self._parse_response(response)
         return schemas.GitlabUser(**response)
 
     async def get_projects(self) -> list[schemas.ProjectAttrs]:
+        """
+        Получение списка активных проектов, в которых пользователь является участником
+        :return: список объектов схемы ProjectAttrs (src.gitlab.schemas.ProjectAttrs)
+        """
         page = 1
         per_page = 100
         result = response = []
@@ -54,9 +80,7 @@ class GitlabAPI:
                     headers=self.headers,
                     params=params
                 )
-                if response.status_code >= 400:
-                    raise GitlabException
-                response = response.json()
+                response = self._parse_response(response)
                 result.extend([schemas.ProjectAttrs(**item) for item in response])
                 page += 1
         return result
@@ -66,10 +90,16 @@ class GitlabAPI:
         url = url.replace('{id}', str(project_id))
         async with httpx.AsyncClient() as session:
             response = await session.get(url, headers=self.headers)
-        if response.status_code >= 400:
-            raise GitlabException
-        response = response.json()
+        response = self._parse_response(response)
         return schemas.ProjectAttrs(**response)
+
+    async def get_webhooks(self, project_id: int) -> list[schemas.HookData]:
+        url = self._get_url(self.Endpoints.list_webhooks)
+        url = url.replace('{id}', str(project_id))
+        async with httpx.AsyncClient() as session:
+            response = await session.get(url, headers=self.headers)
+        response = self._parse_response(response)
+        return [schemas.HookData(**item) for item in response]
 
     async def create_webhook(self, project_id: int, webhook_url: str) -> None:
         url = self._get_url(self.Endpoints.create_webhook)
@@ -83,15 +113,4 @@ class GitlabAPI:
         }
         async with httpx.AsyncClient() as session:
             response = await session.post(url, headers=self.headers, json=data)
-        if response.status_code >= 400:
-            raise GitlabException(response.json())
-
-    async def get_webhooks(self, project_id: int) -> list[schemas.HookData]:
-        url = self._get_url(self.Endpoints.list_webhooks)
-        url = url.replace('{id}', str(project_id))
-        async with httpx.AsyncClient() as session:
-            response = await session.get(url, headers=self.headers)
-        if response.status_code >= 400:
-            raise GitlabException(response.json())
-        response = response.json()
-        return [schemas.HookData(**item) for item in response]
+        self._parse_response(response)
