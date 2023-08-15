66f67cbde220b3455a1c90cba07ab07cb7ef24be
Дмитрий <evans.d@yandex.ru>
2023-08-15 17:44:20 +0300

    Дебаг master

--- src/mattermost/api.py
+++ src/mattermost/api.py
@@ -25,5 +25,6 @@ class MattermostAPI:
         }
         async with httpx.AsyncClient() as session:
             response = await session.post(self._get_url(self.Endpoints.create_post), json=data, headers=self.headers)
+        print(response.json())
         if response.status_code >= 400:
             logging.error(response.json())
