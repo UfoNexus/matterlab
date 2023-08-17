5d7dd36f4b424ac0994fad876315fb7b126fa251
Дмитрий <evans.d@yandex.ru>
2023-08-17 17:44:43 +0300

    Добавил worker для фоновых задач

---
+++ src/config/redis.py
@@ -0,0 +1,3 @@
+from .settings import settings
+
+REDIS_URL = settings.redis_url
--- src/config/settings.py
+++ src/config/settings.py
@@ -6,10 +6,13 @@ class Settings(BaseSettings):
     debug: bool = False
     local: bool = False
 
-    db_host: str
+    db_host: str = 'localhost'
     db_port: int = 5432
     db_user: str
     db_password: str
+    
+    redis_host: str = 'localhost'
+    redis_port: int = 6379
 
     gitlab_secret: str
 
@@ -23,6 +26,10 @@ class Settings(BaseSettings):
     @property
     def db_async_url(self) -> str:
         return f'postgresql+asyncpg://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/postgres'
+    
+    @property
+    def redis_url(self) -> str:
+        return f'redis://{self.redis_host}:{self.redis_port}/0'
 
 
 settings = Settings()
